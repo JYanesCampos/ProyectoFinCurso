a241dc6a8d6ef09e3d1b4b14de37daa5e22504cd
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2023-06-14 19:13:04 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -8,12 +8,4 @@
 //     const PASSWORD = "";
 // }
 
-class Config {
-    //BASE DE DATOS
-    const BD_HOST = "containers-us-west-99.railway.app";
-    const BD_NAME = "railway";
-    const BD_USER = "root";
-    const PASSWORD = "Xe4lFOebXhybN30lcbRV";
-}
-
 ?>
