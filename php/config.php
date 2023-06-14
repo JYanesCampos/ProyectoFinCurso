90fd310a78d3094a1444e286f24fbace31305a58
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2023-06-14 19:41:38 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -2,10 +2,10 @@
 
 class Config {
     //BASE DE DATOS
-    const BD_HOST = ${{MySQL.MYSQLHOST}};
-    const BD_NAME = ${{MySQL.MYSQLDATABASE}};
-    const BD_USER = ${{MySQL.MYSQLUSER}};
-    const PASSWORD = ${{MySQL.MYSQLPASSWORD}};
+    const BD_HOST = ${MySQL.MYSQLHOST};
+    const BD_NAME = ${MySQL.MYSQLDATABASE};
+    const BD_USER = ${MySQL.MYSQLUSER};
+    const PASSWORD = ${MySQL.MYSQLPASSWORD};
 }
 
 ?>
