b1ed57ec40459033d5e9304f65874e10f0035343
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2023-06-14 19:43:35 +0200

    Update config.php

--- php/config.php
+++ php/config.php
@@ -2,10 +2,10 @@
 
 class Config {
     //BASE DE DATOS
-    const BD_HOST = ${MySQL.MYSQLHOST};
-    const BD_NAME = ${MySQL.MYSQLDATABASE};
-    const BD_USER = ${MySQL.MYSQLUSER};
-    const PASSWORD = ${MySQL.MYSQLPASSWORD};
+    const BD_HOST = "containers-us-west-99.railway.app";
+    const BD_NAME = "railway";
+    const BD_USER = "root";
+    const PASSWORD = "Xe4lFOebXhybN30lcbRV";
 }
 
 ?>
