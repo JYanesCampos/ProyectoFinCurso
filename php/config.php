0cafffd240402e94cadaf1a7b1f7968de205c70b
Joaquin Yanes Campos <93703559+JYanesCampos@users.noreply.github.com>
2023-06-15 20:33:31 +0200

    Add files via upload

--- php/config.php
+++ php/config.php
@@ -1,20 +1,25 @@
 <?php
 
-// class Config {
-//     //BASE DE DATOS
-//     const BD_HOST = "localhost";
-//     const BD_NAME = "casa_vacaciones";
-//     const BD_USER = "root";
-//     const PASSWORD = "";
-// }
+/*
+    class Config {
+        //BASE DE DATOS
+        const BD_HOST = "localhost";
+        const BD_NAME = "casa_vacaciones";
+        const BD_USER = "root";
+        const PASSWORD = "";
+    }
+*/
 
-class Config {
-    //BASE DE DATOS
-    const BD_HOST = "containers-us-west-99.railway.app";
-    const BD_NAME = "railway";
-    const BD_USER = "root";
-    const PASSWORD = "Xe4lFOebXhybN30lcbRV";
-    const BD_PORT = "7107";
-}
+
+    class Config {
+        //BASE DE DATOS
+        const BD_HOST = "containers-us-west-99.railway.app";
+        const BD_NAME = "railway";
+        const BD_USER = "root";
+        const PASSWORD = "Xe4lFOebXhybN30lcbRV";
+        const BD_PORT = "7107";
+    }
+
+
 
 ?>
